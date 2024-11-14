afce0c70a15530ff7ab61c7f1bc04711ac183ff4
tanersaydam <tanersaydam@gmail.com>
2024-11-14 13:49:35 +0300

    4.Gün ilk commit

---
+++ TestDrivenDevelopment/4.TDD/eTicaret/src/eTicaret.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace eTicaret.Domain.Entities;
+
+public sealed class User
+{
+    public string FirstName { get; private set; } = default!;
+    public void SetName(string firstName)
+    {
+        if (firstName.Length < 3)
+        {
+            throw new Exception();
+        }
+
+        FirstName = firstName;
+    }
+}
---
+++ TestDrivenDevelopment/4.TDD/eTicaret/tests/eTicaret.Domain.Tests.UnitTest/Entities/UserModelTest.cs
@@ -0,0 +1,34 @@
+using eTicaret.Domain.Entities;
+using FluentAssertions;
+
+namespace eTicaret.Domain.Tests.UnitTest.Entities
+{
+    public class UserModelTest
+    {
+        [Fact]
+        public void FirstName_Should_Throw_Exception_If_Less_Than_3_Characters()
+        {
+            // Arrange
+            User user = new();
+
+            // Act
+            var action = () => user.SetName("Ta");
+
+            // Assert
+            action.Should().Throw<Exception>();
+        }
+
+        [Fact]
+        public void FirstName_Should_Not_Throw_Exception_If_Greater_Than_2_Characters()
+        {
+            // Arrange
+            User user = new();
+
+            // Act
+            var action = () => user.SetName("Taner");
+
+            // Assert
+            action.Should().NotThrow<Exception>();
+        }
+    }
+}
