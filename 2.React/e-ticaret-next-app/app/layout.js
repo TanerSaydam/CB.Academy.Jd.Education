c3e2a2c13b3cf32644be2cc9cd90d120909d67ed
tanersaydam <tanersaydam@gmail.com>
2024-11-21 15:07:23 +0300

    4.Gün 5. ders arası

--- 2.React/e-ticaret-next-app/app/layout.js
+++ 2.React/e-ticaret-next-app/app/layout.js
@@ -1,10 +1,14 @@
+import Script from "next/script";
 import "./globals.css";
+import { Header } from "@/components/header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">      
-      <body>
-        {children}       
+      <body className="container">
+        <Header/>
+        {children}  
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" />     
       </body>
     </html>
   );
